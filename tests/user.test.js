ec4c3b1c2525142d77d28ae8e481a4dd6dd5a3d9
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2023-01-03 15:30:19 +0100

    tests for login a user by phone number

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,12 +2,12 @@ const request = require("supertest");
 const { app } = require("../app");
 
 let user = {
-    username: "Rudy",
-    email: "rudy@test.com",
-    phoneNumber: "1234567890",
-    password: "Test123@",
-    repeatPassword: "Test123@",
-}
+	username: "Rudy",
+	email: "rudy@test.com",
+	phoneNumber: "1234567890",
+	password: "Test123@",
+	repeatPassword: "Test123@",
+};
 
 describe("POST /api/v1/user/signup", () => {
 	it("should return the created user", async () => {
@@ -18,18 +18,18 @@ describe("POST /api/v1/user/signup", () => {
 			expect.objectContaining({
 				status: "created",
 				data: {
-                    _id: expect.any(String),
+					_id: expect.any(String),
 					username: user.username,
 					email: user.email,
 					phoneNumber: user.phoneNumber,
 					bio: "Salut! Lançons un chat!",
-                    createdAt: expect.any(String),
+					createdAt: expect.any(String),
 				},
 			})
 		);
 
-        //For other tests
-        user = res.body.data
+		//For other tests
+		user = res.body.data;
 	});
 
 	it("should return an error for duplicate username", async () => {
@@ -81,17 +81,54 @@ describe("POST /api/v1/user/signup", () => {
 	});
 });
 
-// describe('POST /api/v1/user/login', () => { 
-//     describe('login by phone number', () => {
-//         it('should return the user with access token', async () => {
-//             const res = await request(app).post("/api/v1/user/login").send({
-//                 phoneNumber: user.phoneNumber
-//             })
+describe("POST /api/v1/user/login", () => {
+	describe("login by phone number", () => {
+		it("should return the user with access and refresh tokens", async () => {
+			const res = await request(app).post("/api/v1/user/login").send({
+				phoneNumber: user.phoneNumber,
+			});
 
-//             expect(res.statusCode).toBe(200),
-//             expect(res.body).objectContaining({
+			expect(res.statusCode).toBe(200),
+				expect(res.body).toEqual({
+					status: "success",
+					data: {
+						...user,
+						token: {
+							accessToken: expect.any(String),
+							refreshToken: expect.any(String),
+						},
+					},
+				});
+		});
 
-//             })
-//         });
-//     });
-//  })
+		it("should return the user with access and refresh tokens", async () => {
+			const res = await request(app).post("/api/v1/user/login").send({
+				phoneNumber: user.phoneNumber,
+			});
+
+			expect(res.statusCode).toBe(200),
+				expect(res.body).toEqual({
+					status: "success",
+					data: {
+						...user,
+						token: {
+							accessToken: expect.any(String),
+							refreshToken: expect.any(String),
+						},
+					},
+				});
+		});
+
+		it("should return an error due to unknown phone number", async () => {
+			const res = await request(app).post("/api/v1/user/login").send({
+				phoneNumber: "1231231230",
+			});
+
+            expect(res.statusCode).toBe(404)
+            expect(res.body).toEqual({
+                status: expect.stringContaining("error"),
+                message: expect.stringMatching(/[Nn]on trouvé/)
+            })
+		});
+	});
+});
