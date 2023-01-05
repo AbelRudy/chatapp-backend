f483981b6573741281227eda82c96c56c832680f
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2023-01-05 23:15:22 +0100

    correct login route path

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,6 @@ const UserController = require("../controllers/user.controllers");
 const validator = require("../middlewares/validator")
 
 router.post("/signup", validator("signup"), UserController.signup);
-router.post("/login", validator("login"), UserController.signup);
+router.post("/login", validator("login"), UserController.login);
 
 module.exports = router;
