01cc3405fa67febf0e062920a773215212907fc5
Rudy NDJOUENKEU <nabelrudy@yahoo.fr>
2023-02-07 02:23:23 +0100

    Remove tests

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const { defaults } = require("jest-config");
-
-const config = {
-	...defaults,
-	runTestsByPath: true,
-	setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-};
-
-module.exports = config;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,5 +4,6 @@ const validator = require("../middlewares/validator")
 
 router.post("/signup", validator("signup"), UserController.signup);
 router.post("/login", validator("login"), UserController.login);
+router.post("/logout", validator("logout"), UserController.logout);
 
 module.exports = router;
